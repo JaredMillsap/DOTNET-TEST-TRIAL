7d5acf6f2b01c3fd35ec3efb4e17accd1c721b58
JaredMillsap <jmillsap23@gmail.com>
2021-09-23 13:32:43 -0500

    Learning how to sucessfult use dotnet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace new_folder_trial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is really a test. I'm just learning how to use the dotnet function in the computer");
+        }
+    }
+}
