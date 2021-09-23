1d64fa2ea00a97106f21dda6bb9d43ea4d796987
JaredMillsap <jmillsap23@gmail.com>
2021-09-23 13:47:54 -0500

    minor edits

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace new_folder_trial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is really a test. I'm just learning how to use the dotnet function in the computer");
+            Console.WriteLine("This is really a test. I'm just learning how to use the dotnet function in the compute. This is a slightly different approach. Just making some additional edits.");
         }
     }
+
 }
